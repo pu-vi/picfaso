f29dfdca5f868b1f4ee26b4194e8f69f4f06384d
Vishal Gupta <ims.vishal@gmail.com>
2025-08-11 12:26:56 +0530

    Added util for extracting image info

---
+++ src/utils/json.response.format.ts
@@ -0,0 +1,36 @@
+interface ImageUrls {
+  image?: { url?: string };
+  thumb?: { url?: string };
+  medium?: { url?: string };
+}
+
+interface ExtractedUrls {
+  image: string | null;
+  thumb: string | null;
+  medium: string | null;
+}
+
+const extractUrls = (urls: ImageUrls): ExtractedUrls => ({
+  image: urls.image?.url || null,
+  thumb: urls.thumb?.url || null,
+  medium: urls.medium?.url || null
+});
+
+export const extractFreeImageHostUrls = (data: {
+  image?: ImageUrls;
+}): ExtractedUrls | null => (data?.image ? extractUrls(data.image) : null);
+
+export const extractIbbImageUrls = (data: {
+  data?: ImageUrls;
+}): ExtractedUrls | null => (data?.data ? extractUrls(data.data) : null);
+
+export const extractPhpImageUrls = ({
+  filename = "",
+  path = ""
+}: {
+  filename?: string;
+  path?: string;
+}): { filename: string; path: string } => ({
+  filename,
+  path
+});
